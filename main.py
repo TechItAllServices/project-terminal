5d4de4b7c9697e5e72affc5fbecb88f237030cb5
Jason Guzzetta <benguzzetta@gmail.com>
2023-11-10 22:51:19 -0500

    added ids 3 and 4.

--- main.py
+++ main.py
@@ -43,42 +43,62 @@ def main():
                         print("Invalid command. Please try again.")
             elif command == 'database':
                 maindatabase = input("Enter the data id locater: ")
+                import random
+
                 if maindatabase == "1":
                     from DataBase import CurrentLoggedTVPasswords
                     print("--------------------")
                     print("Current Logged TV Passwords")
                     print("--------------------")
-                    print(f"""{CurrentLoggedTVPasswords.TVPassword1}
-{CurrentLoggedTVPasswords.TVPassword2}
-{CurrentLoggedTVPasswords.TVPassword3}
-{CurrentLoggedTVPasswords.TVPassword4}
-{CurrentLoggedTVPasswords.TVPassword5}
-{CurrentLoggedTVPasswords.TVPassword6}
-{CurrentLoggedTVPasswords.TVPassword7}
-{CurrentLoggedTVPasswords.TVPassword8}
-{CurrentLoggedTVPasswords.TVPassword9}
-{CurrentLoggedTVPasswords.TVPassword10}
-{CurrentLoggedTVPasswords.TVPassword11}
-{CurrentLoggedTVPasswords.TVPassword12}
-{CurrentLoggedTVPasswords.TVPassword13}
-{CurrentLoggedTVPasswords.TVPassword14}
-{CurrentLoggedTVPasswords.TVPassword15}
-{CurrentLoggedTVPasswords.TVPassword16}
-{CurrentLoggedTVPasswords.TVPassword17}
-{CurrentLoggedTVPasswords.TVPassword18}
-{CurrentLoggedTVPasswords.TVPassword19}
-{CurrentLoggedTVPasswords.TVPassword20}
-""")
+                    print(f"""                 {CurrentLoggedTVPasswords.TVPassword1}
+                {CurrentLoggedTVPasswords.TVPassword2}
+                {CurrentLoggedTVPasswords.TVPassword3}
+                {CurrentLoggedTVPasswords.TVPassword4}
+                {CurrentLoggedTVPasswords.TVPassword5}
+                {CurrentLoggedTVPasswords.TVPassword6}
+                {CurrentLoggedTVPasswords.TVPassword7}
+                {CurrentLoggedTVPasswords.TVPassword8}
+                {CurrentLoggedTVPasswords.TVPassword9}
+                {CurrentLoggedTVPasswords.TVPassword10}
+                {CurrentLoggedTVPasswords.TVPassword11}
+                {CurrentLoggedTVPasswords.TVPassword12}
+                {CurrentLoggedTVPasswords.TVPassword13}
+                {CurrentLoggedTVPasswords.TVPassword14}
+                {CurrentLoggedTVPasswords.TVPassword15}
+                {CurrentLoggedTVPasswords.TVPassword16}
+                {CurrentLoggedTVPasswords.TVPassword17}
+                {CurrentLoggedTVPasswords.TVPassword18}
+                {CurrentLoggedTVPasswords.TVPassword19}
+                {CurrentLoggedTVPasswords.TVPassword20}
+                """)
                     print("--------------------")
-                if maindatabase == "2":
+                elif maindatabase == "2":
                     print("--------------------")
                     print(f"""IDENTS""")
                     print("--------------------")
                     print("""Parker: 85jf8v435u34985nbwui23904
-Parker_Old_PC: 08ojgf098id09f9
-Parker_Laptop: 08ojgf098id3224""")
-            else:
-                print("Invalid command. Please try again.")
+                Parker_Old_PC: 08ojgf098id09f9
+                Parker_Laptop: 08ojgf098id3224""")
+                elif maindatabase == "3":
+                    print("--------------------")
+                    print(f"""DATABASE 3""")
+                    print("--------------------")
+                    print(f"""Newly Updated Data VAR 1: {random.randint(1, 100)}
+                Newly Updated Data VAR 2: {random.randint(1, 100)}
+                Newly Updated Data VAR 3: {random.randint(1, 100)}
+                Newly Updated Data VAR 4: {random.randint(1, 100)}
+                Newly Updated Data VAR 5: {random.randint(1, 100)}""")
+                elif maindatabase == "4":
+                    print("--------------------")
+                    print(f"""DATABASE 4""")
+                    print("--------------------")
+                    print(f"""Last Updated Data VAR 1: {random.randint(1, 100)}
+                Last Updated Data VAR 2: {random.randint(1, 100)}
+                Last Updated Data VAR 3: {random.randint(1, 100)}
+                Last Updated Data VAR 4: {random.randint(1, 100)}
+                Last Updated Data VAR 5: {random.randint(1, 100)}""")
+                else:
+                    print("Invalid command. Please try again.")
 
 if __name__ == '__main__':
     main()
